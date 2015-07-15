f65bb756aac4a984954a4ffb5f29f0940e30222d
Cormac Ryan <cormac.ryan@gmail.com>
2015-07-15 12:49:36 +0100

    new file:   LargestTopN.sln 	new file:   LargestTopN.v12.suo 	new file:   LargestTopN/App.config 	new file:   LargestTopN/LargestTopN.cs 	new file:   LargestTopN/LargestTopN.csproj 	new file:   LargestTopN/Program.cs 	new file:   LargestTopN/Properties/AssemblyInfo.cs 	new file:   LargestTopN/numbers.txt

---
+++ LargestTopN/LargestTopN.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LargestTopN
+{
+    public class LargestTopN
+    {
+        public LargestTopN() { }
+
+        /// <summary>
+        /// Read from a potential large file a list of numbers and provide a solution to return back
+        /// an array of the largest top N elements.
+        /// </summary>
+        /// 
+        /// <returns>An simple array of N largest top elements.</returns>
+        public static int[] GetLargestTopN(int topNum)
+        {
+            // Create a top array of elements to hold the return collection of highest numbers
+            List<int> topArray = new List<int>(topNum);
+
+            // Open the FileStream reference
+            using (FileStream fs = File.OpenRead("numbers.txt"))
+            {
+                TextReader sr = new StreamReader(fs, Encoding.UTF8);
+
+                // Read the first topNum elements into the Array
+                for (int i = 0; i < topNum; i++)
+                {
+                    int numberOnLine = Convert.ToInt32(sr.ReadLine());
+                    topArray.Add(numberOnLine);
+                }
+
+                // Sort the ordered elements in descending order highest to lowest
+                topArray.Sort(new Comparison<int>((i1, i2) => i2.CompareTo(i1)));
+
+                // Begin to read the rest of the elements and compare against the current top array
+                while (true)
+                {
+                    string strNumOnLine = sr.ReadLine();
+                    if (strNumOnLine == null)
+                        break;
+
+                    int numberOnLine = Convert.ToInt32(strNumOnLine);
+
+                    // If the number read is less than or equal to the last element in the top array ignore
+                    // as we are only interested in larger elements greater than the last current smallest element
+                    if (numberOnLine <= topArray[topNum - 1])
+                        continue;
+
+                    // Add the element to the current list of array elements
+                    topArray.Add(numberOnLine);              // List Add has a time complexity of O(1)
+
+                    // C# List sorting uses QuickSort which is O(n log n) on average and O(n^2) worst case.
+                    topArray.Sort(new Comparison<int>((i1, i2) => i2.CompareTo(i1)));
+
+                    // Remove the last index element since this is the previous smallest number and it is being 
+                    // kicked out of the current top array due to the new addition above.
+                    topArray.RemoveAt(topArray.Count - 1);   // List Remove has a time complexity of O(n)
+                }
+            }
+
+            return (int[])topArray.ToArray();
+        }
+
+        /// <summary>
+        /// Simple method to generate a file containing a give range of numbers.
+        /// </summary>
+        public static void GenerateNumbersFile()
+        {
+            Random rnd = new Random((int)DateTime.Now.Ticks);
+
+            List<int> ll = new List<int>();
+
+            // Open the FileStream reference
+            using (FileStream fs = File.OpenWrite("numbers.txt"))
+            {                
+                TextWriter sw = new StreamWriter(fs, Encoding.UTF8);
+
+                for (int i=0;  i<10000; i++)
+                {
+                    int rndVal = rnd.Next(1, 200000);
+                    while (ll.Contains(rndVal))
+                    {
+                        rndVal = rnd.Next(1, 204323453);
+                    }
+
+                    ll.Add(rndVal);
+                    sw.WriteLine(rndVal);
+                }
+            }
+        }
+    }
+}
---
+++ LargestTopN/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LargestTopN
+{
+    class Program
+    {
+        /// <summary>
+        /// An algorithm when given an arbitrarily large file containing individual numbers on each line 
+        /// (e.g. 200Gb file) and a number N, will output the largest N numbers from highest first. 
+        /// </summary>
+        static void Main(string[] args)
+        {
+            // Call the algorithm to retrieve the largest top 100 numbers from the local test file.
+            int[] topNArray = LargestTopN.GetLargestTopN(100);
+
+            Console.Out.Write("[");
+            for (int i = 0; i < topNArray.Length; i++)
+            {
+                Console.Out.Write(topNArray[i]);
+                if (i != topNArray.Length - 1)
+                    Console.Out.Write(",");
+            }
+            Console.Out.Write("]");
+        }
+    }
+}
